08ab36c21f0416561728a58fd4735127974c9775
Madumitha Murugesan <madumithamurugesan@gmail.com>
2026-07-31 19:14:35 +0530

    Python Fundamentals

---
+++ Placement/Day 2/Operators.py
@@ -0,0 +1,7 @@
+a = 10
+b = 3
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
---
+++ Placement/Day 3/largest_of_two.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+        print("a is larger than b")
+elif a < b:
+        print("b is larger than a")
+else:
+        print("Both a and b are equal")
